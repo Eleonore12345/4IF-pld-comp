6dfd9793fcb841a45eaaa69ac487168a9c24cbb0
AO2708 <a.pitardbouet@gmail.com>
2025-03-20 10:37:18 +0100

    Warning sur division et modulo par zero

--- compiler/IdentifierVisitor.h
+++ compiler/IdentifierVisitor.h
@@ -15,6 +15,9 @@ class  IdentifierVisitor : public ifccBaseVisitor {
                 virtual antlrcpp::Any visitInitDecla(ifccParser::InitDeclaContext *ctx) override;
                 virtual antlrcpp::Any visitAffectation(ifccParser::AffectationContext *ctx) override;
                 virtual antlrcpp::Any visitVariableSimple(ifccParser::VariableSimpleContext *ctx) override;
+                virtual antlrcpp::Any visitOpMultDiv(ifccParser::OpMultDivContext *ctx) override;
+                virtual antlrcpp::Any visitConstante(ifccParser::ConstanteContext *ctx);
+
         private:
                 SymbolTable* symTable;
                 bool error;
