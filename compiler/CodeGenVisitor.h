1e5c63c0cb5dabd46ab55d1331ce38f340fc7478
qinshu <qinshudai@gmail.com>
2025-03-24 11:31:36 +0100

    change priority

--- compiler/CodeGenVisitor.h
+++ compiler/CodeGenVisitor.h
@@ -22,9 +22,11 @@ class  CodeGenVisitor : public ifccBaseVisitor {
                 virtual antlrcpp::Any visitOpMultDiv(ifccParser::OpMultDivContext *ctx) override;
                 virtual antlrcpp::Any visitOpUnConst(ifccParser::OpUnConstContext *ctx) override;
                 virtual antlrcpp::Any visitOpUnExpr(ifccParser::OpUnExprContext *ctx) override;
-                virtual antlrcpp::Any visitOpBitwise(ifccParser::OpBitwiseContext *ctx) override;
                 virtual antlrcpp::Any visitAffectation(ifccParser::AffectationContext *ctx) override;
                 virtual antlrcpp::Any visitInitDecla(ifccParser::InitDeclaContext * ctx) override;
+                virtual antlrcpp::Any visitOpBitwiseAnd(ifccParser::OpBitwiseAndContext *ctx) override;
+                virtual antlrcpp::Any visitOpBitwiseXor(ifccParser::OpBitwiseXorContext *ctx) override;
+                virtual antlrcpp::Any visitOpBitwiseOr(ifccParser::OpBitwiseOrContext *ctx) override;
 
                 void VariableOrConstante(string name1, string name2);
         private :
